8ab2851175959d225432b92e9f82b3e67705cd0e
mbhadriraju <151967712+mbhadriraju@users.noreply.github.com>
2025-04-04 17:10:08 -0500

    edits

---
+++ components/Surface.jsx
@@ -0,0 +1,33 @@
+import "./components.css";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Plot from 'react-plotly.js'
+
+
+function Surface(props) {
+    if (props.plotData != undefined) {
+        const data = [{
+            x: props.plotData['X'],
+            y: props.plotData['Y'],
+            z: props.plotData['Z'],
+            type: 'scatter3d'
+        }]
+        const layout = {
+            title: {text: 'Volatility Surface'},
+            autosize: true,
+        };
+        return (
+            <div className="plot" id="surface">
+                <Plot data={data} layout={layout}/>
+            </div> 
+        );
+    }
+    else {
+        return (
+            <div className="plot" id="surface">
+            </div>
+        )
+    }
+}
+
+export default Surface;
